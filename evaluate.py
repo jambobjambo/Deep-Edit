7362a5fdb259832b15d5751ed9d52747aef2426d
Jamie McInally <jamiemcinally@Jamies-MacBook-Pro-3.local>
2022-02-15 17:51:10 +0000

    modification

--- evaluate.py
+++ evaluate.py
@@ -9,7 +9,7 @@ from torch import cat, device as get_device, set_grad_enabled
 from torch.cuda import is_available as cuda_available
 from torch.jit import load
 from torchvision.transforms import Compose, Normalize, Resize, ToPILImage, ToTensor
-
+from model import DeepEdit
 # Parse arguments
 parser = ArgumentParser(description="Deep Color: Evaluate")
 parser.add_argument("--model", type=str, default="deep_edit.pt", help="Path to trained model")
@@ -19,6 +19,7 @@ args = parser.parse_args()
 # Load model
 device = get_device("cuda:0") if cuda_available() else get_device("cpu")
 model = load(args.model, map_location=device).eval().to(device)
+print(model)
 set_grad_enabled(False)
 
 # Load image
@@ -30,7 +31,8 @@ input = Image.open(args.input)
 input = to_tensor(input).unsqueeze(dim=0).to(device)
 
 # Run forward
-weights = model.weights(input)
+deepedit = DeepEdit()
+weights = deepedit.weights(input)
 result = model.filter(input, weights)
 
 # Output
